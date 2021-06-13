402a980442bfab5fcf40b9c856444b26c0ab018f
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2021-06-13 18:45:12 +0530

    Update snip_sql.py

--- userbot/plugins/sql_helper/snip_sql.py
+++ userbot/plugins/sql_helper/snip_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, LargeBinary, Numeric, UnicodeText
 
-from AuraXBot.plugins.sql_helper import BASE, SESSION
+from TitanBot.plugins.sql_helper import BASE, SESSION
 
 
 class Snips(BASE):
