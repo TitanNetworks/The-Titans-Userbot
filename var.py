bba95a90042b60ac295e72cd924ffcfcae1f8a24
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2021-06-19 19:57:02 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,9 +1,11 @@
+
 import os
 
 ENV = bool(os.environ.get("ENV", False))
-
 if ENV:
-    from sample_config import Var
+    from heroku_config import Var as Config
 else:
-    if os.path.exists("config.py"):
-        from config import Development as 
+    from local_config import Development as Config
+
+
+Var = Config
